3d4a0909f9fb5fbec30cc17ab2e51b0b819c32dd
Will Burgess <burgeswe@hotmail.com>
2020-08-06 16:52:48 -0500

    Add files via upload
    Script to copy a specified property from all user's Azure AD profiles to their SharePoint profiles

---
+++ CopyCustomAttribute-AzuretoSharepoint.ps1
@@ -0,0 +1,17 @@
+﻿#Variables
+$credUser = "#username"
+$credPass = ConvertTo-SecureString -String "#password" -AsPlainText -Force
+$credential= New-Object -TypeName System.Management.Automation.PSCredential -Argumentlist ($credUser,$credPass)
+$appSiteUrl = "sharepoint admin site url"
+$property = "custom property name"
+
+Connect-PnPOnline -Url $appSiteUrl -Credentials $credential
+Connect-AzureAD -Credential $credential
+$conn = Get-PnPConnection
+ 
+Get-AzureADUser -All $true | ForEach-Object {
+    if ($_.ExtensionProperty.ContainsKey("$property")) {
+        $adValue = $_.ExtensionProperty.Item("$property")
+        Set-PnPUserProfileProperty -Account $_.UserPrincipalName.ToString() -PropertyName $property -Value $adValue
+    }
+}
