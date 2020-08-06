8efc3fc92c3e8d53a86935d6beb70cb4bf60a06a
Will Burgess <burgeswe@hotmail.com>
2020-08-06 17:27:36 -0500

    Add files via upload
    Test Script to Add User Headshots from local file server to Exchange Online

---
+++ CopyUserPicturetoExchangeOnline.ps1
@@ -0,0 +1,16 @@
+﻿#User Images will need to be named the same as the userID
+
+$userPictureFolder = 'folder with user pictures'
+
+Connect-ExchangeOnline
+
+foreach ($picture in Get-ChildItem $userPictureFolder)
+{
+    try
+    {
+        Set-UserPhoto  `
+            -Identity "$picture.BaseName" `
+            -PictureData ([System.IO.File]::ReadAllBytes(“$userPictureFolder\$($picture.Name)”)) `
+            -Confirm:$false
+    }
+}
