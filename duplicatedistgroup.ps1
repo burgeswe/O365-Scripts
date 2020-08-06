5a9ff36fd8a2b819b2a09070da9921ad91f7dc1b
Will Burgess <burgeswe@hotmail.com>
2020-08-06 16:03:55 -0500

    Add files via upload

---
+++ duplicatedistgroup.ps1
@@ -0,0 +1,4 @@
+﻿Set-Variable -Name "DistGroup" -Value "DGName"
+Set-Variable -Name "DistGroupNew" -Value "DGName_New"
+New-DistributionGroup -Name "$DistGroupNew" -DisplayName "$DistGroupNew" -Alias "$DistGroupNew"
+Get-DistributionGroupMember -Identity $DistGroup | % {Add-DistributionGroupMember -Identity "$DistGroupNew" -Member $_.PrimarySmtpAddress}
