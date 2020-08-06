8e000d02601b3f38abf7d5505fdbf60edbf7dcf0
Will Burgess <burgeswe@hotmail.com>
2020-08-06 15:58:43 -0500

    Add files via upload

---
+++ CreateDynamicDistroGroupbyJobTitle.ps1
@@ -0,0 +1,15 @@
+﻿#Add the final name of the Distribution Group to $DistGroupName
+#Add Job Titles 1-3, need to have beginning of title exactly correct, can only wildcard end of the Job Title
+$DistGroupName = ''
+$JobTitle1 = ''
+$JobTitle2 = ''
+$JobTitle3 = ''
+$PrimaryEmail = ''
+
+New-DynamicDistributionGroup -Name $DistGroupName -RecipientFilter "(RecipientType -eq 'UserMailbox') -and (Title -like '$JobTitle1*' -or Title -like '$JobTitle2*' -or Title -like '$JobTitle3*')" -PrimarySmtpAddress "$PrimaryEmail"
+
+#Set Variable $DynDistMembers to pull users in new Dynamic Distro
+$DynDistMembers = Get-DynamicDistributionGroup $DistGroupName
+
+#Show users in new Dynamic Distro
+Get-Recipient -RecipientPreviewFilter $DynDistMembers.RecipientFilter -OrganizationalUnit $DynDistMembers.RecipientContainer
