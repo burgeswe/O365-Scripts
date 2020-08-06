4e5eba853f3984065cc45f39efbe3a034b7e556a
Will Burgess <burgeswe@hotmail.com>
2020-08-06 17:46:27 -0500

    Add files via upload
    Add one user or a group of users (by email) to a group of user's calendars

---
+++ AddUsertoGroupMemberCalendars.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module ActiveDirectory
+
+$calendars = Get-ADGroupMember -Identity "owners" | Where objectclass -eq 'user' | Get-ADUser -Properties samAccountName,mail | select samAccountName,mail
+$reviewerEmailAddress = 'email address of user or group to add as reviewers'
+#AccessRights Variable can be set to a list of things listed at https://docs.microsoft.com/en-us/powershell/module/exchange/add-mailboxfolderpermission?view=exchange-ps
+$AccessRights = Reviewer
+
+Connect-ExchangeOnline
+
+ForEach ($user in $calendars)
+    {
+    Add-MailboxFolderPermission (""+ $user.mail+ ":\Calendar") -User $reviewerEmailAddress -AccessRights $AccessRights
+    }
